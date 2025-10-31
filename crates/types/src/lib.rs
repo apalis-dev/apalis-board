2057077aa12a7de260c0d96b9bdfbed26d5703b6
geofmureithi <mureithinjuguna@gmail.com>
2025-10-31 10:25:00 +0300

    include README symlink

--- crates/types/src/lib.rs
+++ crates/types/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/../../README.md"))]
+#![doc = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/README.md"))]
 use serde::{Deserialize, Serialize};
 
 #[derive(Default, Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
